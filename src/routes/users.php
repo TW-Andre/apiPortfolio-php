d4833497cec80119da7219c779fddf6d98a23bf9
André dos Santos <andredossantosjp@gmail.com>
2026-05-24 10:38:52 -0300

    update else

--- src/routes/users.php
+++ src/routes/users.php
@@ -7,4 +7,7 @@ $controller = new UserController($pdo);
 if ($method === 'GET') {
     $users = $controller->getAll();
     echo json_encode($users);
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Método não permitido']);
 }
